49d485df0342ad6bedb65b10ffe3dc78e381f40a
StufMuff <stuff_j@yahoo.com>
2025-05-09 15:53:49 -0500

    Added the restriction of how straight the robot will move forward using a variable driving_heading_tolerance.

--- remote_launch.sh
+++ remote_launch.sh
@@ -61,6 +61,32 @@ for robot_num in "$@"; do
             chmod +x start_node.sh start_mocap.sh test_node.sh
 EOF
         ;;
+    "deepBuild")
+        echo "Building workspace at $pi_ip..."
+        ssh ${USER}@${pi_ip} << EOF
+
+            source /opt/ros/humble/setup.bash
+            source /etc/turtlebot4/setup.bash
+
+            cd $pi_wrk_space
+
+            # Loop through all Python files
+            for file in ./src/agent_control/agent_control/*.py; do
+                # Check if the file exists (in case there are no .py files)
+                if [ "\$file" != "./src/agent_control/agent_control/*.py" ]; then
+                    # Mark the file as executable
+                    chmod +x "\$file"
+                    echo "Marked \$file as Executable"
+                else
+                    echo "No Python Files Found!"
+                fi
+            done
+            
+            echo "Building..."
+            colcon build
+            chmod +x start_node.sh start_mocap.sh test_node.sh
+EOF
+        ;;
 
     "run")
         echo "Running ROS2 nodes at $pi_ip..."
