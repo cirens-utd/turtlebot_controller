0cdf34e612bd9f11df4717b05bbd5b93f860553f
StufMuff <stuff_j@yahoo.com>
2025-08-27 16:13:00 -0500

    Update Mocap IP Address

--- start_mocap.sh
+++ start_mocap.sh
@@ -5,5 +5,5 @@ source /etc/turtlebot4/setup.bash
 
 if [ $(ps aux | grep vrpn | wc -l) -le 1 ]; then
 	#screen -dmS ros_session1 bash -c "ros2 launch vrpn_mocap client.launch.yaml server:=192.168.0.131 port:=3883 update_freq:=180"
-	tmux new-session -d -s ros_session1 "bash -c 'ros2 launch vrpn_mocap client.launch.yaml server:=192.168.0.131 port:=3883 update_freq:=180'"
+	tmux new-session -d -s ros_session1 "bash -c 'ros2 launch vrpn_mocap client.launch.yaml server:=192.168.0.3 port:=3883 update_freq:=180'"
 fi
