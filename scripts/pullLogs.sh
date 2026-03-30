cf713d167b89204088f4bd9c75ba00a825da6e36
StufMuff <stuff_j@yahoo.com>
2026-03-30 13:19:34 -0500

    Error in log file name

--- scripts/pullLogs.sh
+++ scripts/pullLogs.sh
@@ -4,7 +4,7 @@ USER=$1
 pi_ip=$2
 pi_wrk_space=$3
 robot_num=$4
-robot_name="Robot$robot_num_"
+robot_name="Robot${robot_num}_"
 
 echo "Coping Replays From $pi_ip..."
 scp -r ${USER}@${pi_ip}:${pi_wrk_space}/Replays ./
