7f0d5cb039f1a7f5d276c0f7bc76a42cad9a1e7e
StufMuff <stuff_j@yahoo.com>
2026-03-30 12:57:52 -0500

    Also pull log.txt file

--- scripts/pullLogs.sh
+++ scripts/pullLogs.sh
@@ -3,6 +3,11 @@
 USER=$1
 pi_ip=$2
 pi_wrk_space=$3
+robot_num=$4
+robot_name="Robot$robot_num_"
 
 echo "Coping Replays From $pi_ip..."
 scp -r ${USER}@${pi_ip}:${pi_wrk_space}/Replays ./
+
+echo "Coping Log File from $pi_ip..."
+scp ${USER}@${pi_ip}:${pi_wrk_space}/log.txt ./${robot_name}log.txt
